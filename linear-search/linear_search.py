454514a5d9c9d0545fe1c26c0fa30a4ebac4c853
temirlanA <50235840+temirlanA@users.noreply.github.com>
2020-10-15 22:25:46 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,6 +1,6 @@
 words = {"keyboard":"клавиатура","mouse":"мышка","computer":"компьютер","printer":"принтер","monitor":"монитор"}
 def linear_search(lst, to_find):
-  i=0#iterator starts from 0
+  i=0#index starts from 0
   while (len(lst.keys()) > 0):
     if(list(lst.keys())[i] == to_find):
       return i
