04051c6fc6b26d9fc9435a9297bdb915eee31563
temirlanA <50235840+temirlanA@users.noreply.github.com>
2020-10-15 22:25:21 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -6,3 +6,8 @@ def linear_search(lst, to_find):
       return i
     i += 1
   return -1
+
+print(linear_search(words,"computer"))
+  # search for the element to_find inside lst
+  # if found, return index of element
+  # else return -1
